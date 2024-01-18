df82628c505246b667527ee67fd1db95547d0aee
TOP-FDesmet <florian.desmet@topchretien.com>
2024-01-18 16:38:59 +0100

    UI appBar && menuNav

--- App.tsx
+++ App.tsx
@@ -1,86 +1,104 @@
-import React, {Component} from 'react';
-import {ScrollView, Text, View, Image, Button} from 'react-native';
-
-const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-  width: 64,
-  height: 64,
-};
+import React, {} from 'react';
+import { 
+  View, 
+  StyleSheet, 
+  Text, 
+  Image, 
+  Pressable, 
+  Alert } from 'react-native';
 
 const App = () => {
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'space-between',
-    }}>
-      <View style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-        backgroundColor: 'lavenderblush',
-      }}>
-        <Text style={{fontSize: 30, fontWeight: '700', color: 'black'}}>PassLeMot</Text>
+    <View style={styles.container}>
+
+      {/* appbar */}
+      <AppBar />
+
+      {/* page content */}
+      <View style={styles.pageContent}>
+
       </View>
 
-      <View style={{
-        flex: 10,
-        padding: 1,
-        alignItems: 'center',
-        width: '100%',
-        backgroundColor: 'lemonchiffon',
-      }}>
-        <ScrollView style={{
-          width: '100%',
-          borderColor: 'white',
-          borderWidth: 3,
-        }}>
-          <Text style={{fontSize: 96}}>Scroll me plz</Text>
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Text style={{fontSize: 96}}>If you like</Text>
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Text style={{fontSize: 96}}>Scrolling down</Text>
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Text style={{fontSize: 96}}>What's the best</Text>
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Text style={{fontSize: 96}}>Framework around?</Text>
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Image source={logo} />
-          <Text style={{fontSize: 80}}>React Native</Text>
-        </ScrollView>
-      </View>
+      {/* menu nav */}
+      <MenuNav />
 
+    </View>
+  );
+};
+
+const AppBar = () => {
+  return (
+    <View style={styles.appBar}>
       <View style={{
-        flex: 1,
         flexDirection: 'row',
-        justifyContent: 'space-around',
-        width: '100%',
-        backgroundColor: 'lightblue',
-      }}>
-        <Button title='Accueil' />
-        <Button title='A propos' />
+        justifyContent: 'space-between',
+        alignItems: 'center',}}>
+        
+        {/* menu burger */}
+        <Pressable onPress={() => Alert.alert('Menu')}>
+          <Image source={require('./img/menu-scale.png')} />
+        </Pressable>
+        
+        <Text style={{fontSize: 18, fontWeight: '500', color: 'black'}}>Title</Text>
+        
+        {/* profil */}
+        <Pressable onPress={() => Alert.alert('Profil')}>
+          <Image style={{
+            borderRadius: 50, 
+            borderWidth: 2, 
+            borderColor: 'white'}} source={require('./img/avatar.png')} />
+        </Pressable>
       </View>
     </View>
   );
 };
+
+const MenuNav = () => {
+  return (
+    <View style={styles.menuNav}>
+      <Pressable onPress={() => Alert.alert('Home')}>
+          <Image source={require('./img/home-simple-door.png')} />
+      </Pressable>
+      <Pressable onPress={() => Alert.alert('Favoris')}>
+          <Image source={require('./img/heart.png')} />
+      </Pressable>
+      <Pressable onPress={() => Alert.alert('Daily')}>
+          <Image source={require('./img/journal.png')} />
+      </Pressable>
+    </View>
+  );
+};
+
+// stylesheet
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'space-between',
+    height: '100%',
+    backgroundColor: 'lightblue',
+  },
+  appBar: {
+    justifyContent: 'center',
+    flexGrow: 0,
+    width: '100%',
+    height: 90,
+    paddingHorizontal: 20,
+    backgroundColor: 'white',
+  },
+  pageContent: {
+    flexGrow: 1,
+    width: '100%',
+  },
+  menuNav: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexGrow: 0,
+    width: '100%',
+    height: 78,
+    backgroundColor: 'white',
+    borderTopStartRadius: 39,
+    borderTopRightRadius: 39,
+  },
+});
 
 export default App;
